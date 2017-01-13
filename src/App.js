2e675557b0e977bdf3f6c5b2a67d8bdafc3e8c59
David Murray <dmurraykauai@gmail.com>
2017-01-13 11:51:53 -1000

    completed course

--- src/App.js
+++ src/App.js
@@ -1,43 +1,133 @@
 import React from 'react';
 
+// ** lesson 21 - Debug React Components with Developer Tools in Chrome
+
+
+
+// ** lesson 20 - Write More Reusable React Components with Composable APIs
+
+// jsbin code doesn't work... moving on ...
+// import ReactDOM from 'react-dom';
+//
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             red: 0
+//         };
+//         this.update = this.update.bind(this)
+//     }
+//
+//     update(e) {
+//         this.setState({
+//             red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
+//         })
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <NumInput
+//                     ref="red"
+//                     min={0}
+//                     max={255}
+//                     step={0.01}
+//                     val={+this.state.red}
+//                     type="number"
+//                     label="Red"
+//                     update={this.update}/>
+//             </div>
+//         );
+//     }
+// }
+//
+// class NumInput extends React.Component {
+//     render() {
+//         let label = this.props.label !== '' ?
+//             <label>{this.props.label} - {this.props.val}</label> : ''
+//         return (
+//             <div>
+//                 <input ref="inp"
+//                        type={this.props.type}
+//                        min={this.props.min}
+//                        max={this.props.max}
+//                        step={this.props.step}
+//                        defaultValue={this.props.val}
+//                        onChange={this.props.update}/>
+//                 {label}
+//             </div>
+//         );
+//     }
+// }
+//
+// NumInput.propTypes = {
+//     min: React.PropTypes.number,
+//     max: React.PropTypes.number,
+//     step: React.PropTypes.number,
+//     val: React.PropTypes.number,
+//     label: React.PropTypes.string,
+//     update: React.PropTypes.func.isRequired,
+//     type: React.PropTypes.oneOf(['number', 'range'])
+// };
+//
+// NumInput.defaultProps = {
+//     min: 0,
+//     max: 0,
+//     step: 1,
+//     val: 0,
+//     label: '',
+//     type: 'range'
+// };
+//
+//
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
 // ** lesson 19 - Use React.cloneElement to Extend Functionality of Children Components
 
-class App extends React.Component {
-    render() {
-        return(
-            <Buttons>
-                <button value="A">A</button>
-                <button value="B">B</button>
-                <button value="C">C</button>
-            </Buttons>
-        )
-    }
-}
-
-class Buttons extends React.Component {
-    constructor() {
-        super();
-        this.state = { selected: 'None' }
-    }
-    selectItem(selected) {
-        this.setState({selected})
-    }
-    render() {
-        let fn = child =>
-            React.cloneElement(child, {
-                onClick: this.selectItem.bind(this, child.props.value)
-            });
-        let items = React.Children.map(this.props.children, fn);
-        return(
-            <div>
-                <h2>You have selected: {this.state.selected}</h2>
-                {items}
-            </div>
-        )
-    }
-}
-
-export default App
+// class App extends React.Component {
+//     render() {
+//         return(
+//             <Buttons>
+//                 <button value="A">A</button>
+//                 <button value="B">B</button>
+//                 <button value="C">C</button>
+//             </Buttons>
+//         )
+//     }
+// }
+//
+// class Buttons extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             selected: 'None',
+//             btnColor: ''
+//         }
+//     }
+//     selectItem(selected) {
+//         this.setState({selected});
+//         console.log(selected);
+//
+//     }
+//     render() {
+//         let fn = child =>
+//             React.cloneElement(child, {
+//                 onClick: this.selectItem.bind(this, child.props.value)
+//             });
+//         let items = React.Children.map(this.props.children, fn);
+//         return(
+//             <div>
+//                 <h2>You have selected: {this.state.selected}</h2>
+//                 {items}
+//             </div>
+//         )
+//     }
+// }
+//
+// export default App
 
 
 // ** lesson 18 - Understand React.Children Utilities
@@ -125,8 +215,6 @@ export default App
 // }
 //
 // export default App
-
-
 
 
 // ** lesson 15 - Compose React Component Behavior with Higher Order Components
